2e19d36c661239ccb1e52c3bee6b476753aed135
Antikrizisss <78258378+Antikrizisss@users.noreply.github.com>
2021-04-20 16:42:55 +0400

    Добавьте файлы проекта.

---
+++ WpfCalc/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCalc
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Application.Current.Properties["GodMode"] = false;
+            foreach (string arg in e.Args)
+            {
+                if (arg.Equals("/godmode", StringComparison.OrdinalIgnoreCase))
+                {
+                    Application.Current.Properties["GodMode"] = true;
+                    break;
+                }
+            }
+            MessageBox.Show("Привет, это мой первый калькулятор WPF");
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+        }
+    }
+}
---
+++ WpfCalc/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_Closed;
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string msg = "Вы действительно хотите выйти?";
+            MessageBoxResult result = MessageBox.Show(msg, "Калькулятор", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пока!");
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Щелчок!");
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            //this.Title = e.GetPosition(this).ToString();
+        }
+    }
+}
